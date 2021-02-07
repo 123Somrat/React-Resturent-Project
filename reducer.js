6ebdf9ec50aac5e6e60d5fbc42e3e1650c259506
123Somrat <77213623+123Somrat@users.noreply.github.com>
2021-02-08 02:33:45 +0600

    Add files via upload

---
+++ reducer.js
@@ -0,0 +1,20 @@
+import DISHES from "../data/dishes"
+import COMMENTS from "../data/comments"
+
+
+const initstate = {
+    dish: DISHES,
+    comments: COMMENTS,
+
+
+
+}
+
+
+
+
+export const Reducer = (state = initstate, action) => {
+    return state;
+
+
+}
